8e8228aab06cd0eeffedb5336a931a720a08f9da
BShrestha <41086302+ijyapu@users.noreply.github.com>
2023-05-11 16:40:24 -0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,3 @@
-/***********************************************
-Author: Bikash Shrestha
-Date: 04/10/2023
-Purpose: The purpose of this program is to use override functions in order to play a number guessing game that we made earlier this year 
-         and have the player play against a computer as well as the computer vs computer. 
-Sources of Help: Class Slides/Worksheet and Absolute C++ (6th edition), by Walter Savitch, Kenrick Mock, Pearson.
-Time Spent: 3 hours
-***********************************************/
-/*
-Computing III -- COMP.2010 Honor Statement
-The practice of good ethical behavior is essential for maintaining
-good order in the classroom, providing an enriching learning
-experience for students, and as training as a practicing computing
-professional upon graduation. This practice is manifested in the
-University’s Academic Integrity policy. Students are expected to
-strictly avoid academic dishonesty and adhere to the Academic
-Integrity policy as outlined in the course catalog. Violations will
-be dealt with as outlined therein.
-All programming assignments in this class are to be done by the
-student alone. No outside help is permitted except the instructor and
-approved tutors.
-I certify that the work submitted with this assignment is mine and was
-generated in a manner consistent with this document, the course
-academic policy on the course website on Blackboard, and the UMass
-Lowell academic code.
-Date: 04/10/2023
-Name: Bikash Shrestha
-*/
 
 #include <iostream>
 #include <cstdlib>
